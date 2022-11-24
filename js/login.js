a4a35aab5ce758726d2b4dbf85a4f301deeb74b0
keviin999 <kevind2811@gmail.com>
2022-11-23 21:47:05 -0300

    Google

--- js/login.js
+++ js/login.js
@@ -12,3 +12,16 @@ document.getElementById("validar").addEventListener("submit", function(a){
 })
 
 
+function jwtDecode(res) {
+    let user = decode(res.credential)
+    localStorage.setItem("usuario", user.email)
+    window.location.href = "inicio.html"
+}
+function decode(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+};
